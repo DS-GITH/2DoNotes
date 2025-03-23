28935fd8265693643a7c2f61c7f272a724ffc586
David Santos <david.tecds@gmail.com>
2025-03-23 08:57:07 -0300

    Add Adding page component with basic structure and placeholder text

---
+++ 2DN/src/pages/Adding/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+export default function Adding() {
+
+return(
+    <View style={styles.container}>
+        <Text> Opa </Text>
+    </View>
+)
+};
+
+const styles = StyleSheet.create({
+    container: {
+        
+    },
+});
